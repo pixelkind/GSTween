6c437b12b9f9304aed5e9c9bb93bc4744f535bc3
pixelkind <github@pixelkind.com>
2014-02-06 12:18:32 +0100

    Initial Commit

---
+++ GSTween/GSTween.h
@@ -0,0 +1,13 @@
+//
+//  GSTween.h
+//  GSTween
+//
+//  Created by Garrit Schaap on 06.02.14.
+//  Copyright (c) 2014 Garrit Schaap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSTween : NSObject
+
+@end
---
+++ GSTween/GSTween.m
@@ -0,0 +1,13 @@
+//
+//  GSTween.m
+//  GSTween
+//
+//  Created by Garrit Schaap on 06.02.14.
+//  Copyright (c) 2014 Garrit Schaap. All rights reserved.
+//
+
+#import "GSTween.h"
+
+@implementation GSTween
+
+@end
